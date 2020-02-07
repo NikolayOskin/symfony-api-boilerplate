5082e81cf3a647f782c54f09ba18efb4b838e83c
Oskin <wskeal@gmail.com>
2020-02-08 00:18:31 +0300

    migrate routes to controller annotations

--- src/Controller/Auth/ConfirmRegistrationController.php
+++ src/Controller/Auth/ConfirmRegistrationController.php
@@ -7,25 +7,37 @@ use App\Model\User\UseCase\ConfirmSignUp\ConfirmSignUpCommand;
 use App\Model\User\UseCase\ConfirmSignUp\ConfirmSignUpHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class ConfirmRegistrationController extends AbstractController
 {
+    /**
+     * @Route("/auth/confirm", name="auth.confirm", methods={"POST"})
+     *
+     * @param Request $request
+     * @param ValidatorInterface $validator
+     * @param ConfirmSignUpHandler $handler
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
     public function index(
         Request $request,
         ValidatorInterface $validator,
         ConfirmSignUpHandler $handler
     ) {
-        $command = new ConfirmSignUpCommand($request->get('email'), $request->get('confirmToken'));
+        $command = new ConfirmSignUpCommand(
+            $request->get('email') ?? '',
+            $request->get('confirmToken') ?? ''
+        );
         $errors = $validator->validate($command);
 
         if (count($errors) > 0) {
             $errors = new ValidationErrors($errors);
-            return $this->json(['errors' => $errors->toArray()]);
+            return $this->json(['errors' => $errors->toArray()], 422);
         }
 
         $handler->handle($command);
 
-        return $this->json(['success' => 'Registration confirmed']);
+        return $this->json(['success' => 'Registration confirmed'], 200);
     }
 }
--- src/Controller/Auth/SignUpController.php
+++ src/Controller/Auth/SignUpController.php
@@ -8,29 +8,38 @@ use App\Model\User\UseCase\SignUp\SignUpHandler;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class SignUpController extends AbstractController
 {
+    /**
+     * @Route("/auth/signup", name="auth.signup", methods={"POST"})
+     *
+     * @param SignUpHandler $handler
+     * @param Request $request
+     * @param ValidatorInterface $validator
+     * @return JsonResponse
+     */
     public function index(
         SignUpHandler $handler,
         Request $request,
         ValidatorInterface $validator
     ) : JsonResponse {
         $command = new SignUpCommand(
-            $request->get('email'),
-            $request->get('password')
+            $request->get('email') ?? '',
+            $request->get('password') ?? ''
         );
 
         $errors = $validator->validate($command);
 
         if (count($errors) > 0) {
             $errors = new ValidationErrors($errors);
-            return $this->json(['errors' => $errors->toArray()]);
+            return $this->json(['errors' => $errors->toArray()], 422);
         }
 
         $handler->handle($command);
 
-        return $this->json(['success' => $command->email]);
+        return $this->json(['success' => $command->email], 200);
     }
 }
